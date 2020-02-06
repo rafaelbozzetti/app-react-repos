12babe95d22d3c226393f13f0e633521b7c69673
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-02-05 22:09:48 -0300

    aplicação reactjs

---
+++ src/pages/Main/index.js
@@ -0,0 +1,62 @@
+import React, { Component }  from 'react';
+
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+class Main extends Component {
+
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const { newRepo } = this.state;
+    const response = await api.get(`repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    }
+    this.setState({
+      repositories: [...this.state.repositories, data],
+      newRepo: '',
+      loading: true,
+    })
+  }
+
+  render() {
+
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            { loading ? <FaSpinner color="#fff" size={14} /> : <FaPlus color="#fff" size={14} /> }
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default Main;
