9508dd69f1239eac66da65205c492274d5d90153
Rafael Pereira Bozzetti <rafaelbozzetti@gmail.com>
2020-02-06 18:17:20 -0300

    Tela de informações do repositório

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component }  from 'react';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
